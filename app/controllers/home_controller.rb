0b045e4410405eb6e427cb61598eb65d4e229870
Jeremiah Hemphill <jeremiah@cloudspace.com>
2014-10-30 09:54:38 -0400

    Added the ability to tweet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,6 @@ class HomeController < ApplicationController
 
   def tweet
     if current_user
-      client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = Rails.application.secrets.twitter_app_id
-        config.consumer_secret     = Rails.application.secrets.twitter_app_secret
-        config.access_token        = current_user.identity.token
-        config.access_token_secret = current_user.identity.secret
-      end
-
       client.user_timeline.each do |tweet|
         if Tweet.where(:twitter_id => tweet.id).empty?
           t = Tweet.create!(:twitter_id => tweet.id, :full_text => tweet.full_text, :tweeted_at => tweet.created_at)
@@ -21,9 +14,29 @@ class HomeController < ApplicationController
         end
       end
 
-      @tweets = Tweet.all
+      @tweets = Tweet.order(:tweeted_at => :desc)
     else
       redirect_to root_path
     end
+  end
+
+  def create_tweet
+    client.update(params['Full Text'])
+  end
+
+  private
+
+  def client
+    if current_user
+      unless defined? @client
+        @client = Twitter::REST::Client.new do |config|
+          config.consumer_key        = Rails.application.secrets.twitter_app_id
+          config.consumer_secret     = Rails.application.secrets.twitter_app_secret
+          config.access_token        = current_user.identity.token
+          config.access_token_secret = current_user.identity.secret
+        end
+      end
+    end
+    @client
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
   get '/tweet' => 'home#tweet'
+  post '/tweet' => 'home#create_tweet'
   root to: "home#index"
 end
